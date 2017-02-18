028224ef73bfbc7f4c2167360f7b219feb34e7c8
Chris Kelly <bigkahuna1uk-git@gmail.com>
2017-02-18 18:56:58 +0000

    Added function to class
    - Added Guava dependency to check interop

--- src/main/kotlin/com/chriskelly/kotlincourse/classes/Classes.kt
+++ src/main/kotlin/com/chriskelly/kotlincourse/classes/Classes.kt
@@ -9,16 +9,16 @@ import java.util.*
  * @author bigkahuna
  * @since 18/02/2017
  */
-class Customer1(val id: Int, var name: String) {
+class Customer1(val id: Int, var name: String, var email: String = "") {
     init {
         //Do some extra processing on var
+        require(id > 0) { "id must be greater than zero" }
         name = name.toUpperCase()
+        email = email.toUpperCase()
     }
 
     @JvmOverloads
-    constructor(email: String = "") : this(0, "") {
-        //FIXME: How to make email a property?
-    }
+    constructor(email: String) : this(1, "Unknown", email)
 }
 
 class Customer(val id: Int, var name: String, val yearOfBirth: Int) {
@@ -26,22 +26,28 @@ class Customer(val id: Int, var name: String, val yearOfBirth: Int) {
         get() = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth
 
     var ssn: String = ""
-    set(value) {
-        if(!value.startsWith("SN")) {
-            throw IllegalArgumentException("Social security should start with SN")
+        set(value) {
+            if (!value.startsWith("SN")) {
+                throw IllegalArgumentException("Social security should start with SN")
+            }
+
+            field = value
         }
 
-        field = value
+    fun asString(): String {
+        return "Customer: id = $id, name=$name, age=$age, ssn=$ssn"
     }
 }
 
 
 fun main(args: Array<String>) {
-    val c = Customer1(10, "Chris")
+    val c1 = Customer1(10, "Chris")
     val c2 = Customer1("a@bc.co.uk")
 
-    println(c.name)
-    println("c = ${c}")
+    println(c2.name)
+    println(c2.email)
+
+    println("c = ${c1}")
 
     val customer = Customer(10, "Chris", 1980)
     customer.ssn = "SN1234"
@@ -51,5 +57,6 @@ fun main(args: Array<String>) {
     println("customer.age = ${customer.age}")
     println("customer.ssn = ${customer.ssn}")
 
+    println(customer.asString())
 
 }
