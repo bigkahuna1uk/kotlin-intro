1c847692e2aef4c967471ed025c13d4a33325d88
Chris Kelly <bigkahuna1uk-git@gmail.com>
2017-03-05 19:12:21 +0000

    Lambda examples
    - Inline lambdas
    - Lambdas applied to a value
    - Lambdas as last parameter to fun -> micro DSLs

--- src/main/kotlin/com/chriskelly/kotlincourse/functional/GettingFunctional.kt
+++ src/main/kotlin/com/chriskelly/kotlincourse/functional/GettingFunctional.kt
@@ -23,14 +23,40 @@ fun <T> operationT(x: T, op: (T) -> T): T {
 
 
 fun route(path: String, vararg actions: (String, String) -> String) {
-    for(action in actions)
-    {
+    for (action in actions) {
         action(path, ",")
     }
 }
 
 fun sum(x: Int, y: Int) = x + y
 
+fun unaryOperation(x: Int, op: (Int) -> Int): Int {
+    return op(x)
+}
+
+fun unaryOp(op: (Int) -> Int) {
+    
+}
+
+fun transaction(db: Database, log: Logger, code: () -> Unit) {
+    try {
+        code()
+        log.info("Code run complete")
+    } finally {
+        db.commit()
+        log.info("Code committed to database")
+    }
+}
+
+class Database {
+    fun commit() {}
+}
+
+class Logger() {
+    fun info(msg: String) {
+        println(msg)
+    }
+}
 
 fun main(args: Array<String>) {
     val result = operation(2, 3, ::sum)
@@ -39,4 +65,32 @@ fun main(args: Array<String>) {
     println(operation(1, ::identity))
     
     println(operationT("Chris Kelly woz ere", ::identity))
+    
+    val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+    val resultWithLambda = operation(2, 3, sumLambda)
+    println("resultWithLambda = ${resultWithLambda}")
+    
+    unaryOperation(2, { x -> x * x })
+    
+    //If single parameter can use 'it'
+    unaryOperation(2, { it * it })
+    
+    //If last/only parameter is function then can be expressed differently. Useful for DSLs
+    unaryOperation(2) { it * it }
+    
+    unaryOp {
+        // Looks like a keyword
+        it * it
+    }
+    
+    val db = Database()
+    val logger = Logger()
+    
+    transaction(db, logger) {
+        //Interact with database, logger etc
+    }
+    
+    //Anonymous function - can have multiple return points unlike lambdas
+    unaryOperation(3, fun(x: Int): Int { return x * x })
+    
 }
