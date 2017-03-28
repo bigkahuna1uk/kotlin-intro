b06797ec216316772bbf5ea19b188e7db6837fff
Chris Kelly <bigkahuna1uk-git@gmail.com>
2017-03-28 07:36:48 +0100

    Kotlin interop Java

--- src/main/kotlin/com/chriskelly/kotlincourse/classes/CustomerKotlin.kt
+++ src/main/kotlin/com/chriskelly/kotlincourse/classes/CustomerKotlin.kt
@@ -3,6 +3,7 @@ package com.chriskelly.kotlincourse.classes
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.assertj.core.api.Assertions.assertThat
+import java.io.IOException
 
 /**
  * CustomerKotlin
@@ -11,11 +12,34 @@ import org.assertj.core.api.Assertions.assertThat
  * @author bigkahuna
  * @since 18/02/2017
  */
-data class CustomerKotlin(var id: Int, var name: String, var email: String)
-{
+data class CustomerKotlin(var id: Int, var name: String, var email: String) {
+    @JvmField val someProperty = "Value"
+    
+    //Create overload methods with and without default parameter
+    @JvmOverloads fun changeStatus(status: Status = Status.Current) {
+        
+    }
+    
+    @JvmName("preferential") fun makePreferred() {
+        
+    }
+    
+    //Kotlin does not have checked exceptions but can annotate that it does throw
+    @Throws(IOException::class) fun loadStats(filename: String) {
+        if ("" == filename) {
+            throw IOException("Filename must be specified")
+        }
+    }
+    
     override fun toString(): String {
         return "{id:\"$id\" name:\"$name\" email:\"$email\"}"
     }
+}
+
+enum class Status {
+    Current,
+    Past
+    
 }
 
 
@@ -24,7 +48,7 @@ fun main(args: Array<String>) {
     val customer2 = CustomerKotlin(1, "Chris", "chris.kelly@ab.co.uk")
     val customer3 = CustomerKotlin(2, "Chris", "chris.kelly@ab.co.uk")
     val customerCopy = customer1.copy()
-
+    
     println("customer1 = $customer1")
     println("customer2 = $customer2")
     println("customer3 = $customer3")
@@ -36,11 +60,11 @@ fun main(args: Array<String>) {
     println()
     println("customer1 identity ${System.identityHashCode(customer1)}")
     println("cust copy identity ${System.identityHashCode(customerCopy)}")
-
+    
     println()
     val customerCopyWithDifferentName = customer1.copy(name = "joe.bloggs")
     println("customerCopyWithDifferentName = $customerCopyWithDifferentName")
-
+    
     val mapper = ObjectMapper().registerModule(KotlinModule())
     val json = mapper
             .writerWithDefaultPrettyPrinter()
@@ -49,6 +73,6 @@ fun main(args: Array<String>) {
     val deserialized = mapper.readValue(json, CustomerKotlin::class.java)
     assertThat(deserialized).isEqualTo(customerCopyWithDifferentName)
     assertThat(deserialized).isNotSameAs(customerCopyWithDifferentName)
-
-
+    
+    
 }
